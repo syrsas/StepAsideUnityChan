44e3a9b3b3004fca6e0169dade9d43cc0bcf41fb
syrsas <87002583+syrsas@users.noreply.github.com>
2021-08-02 23:45:26 +0900

    ファイル作成
    ユニティーちゃん

--- Assets/UnityChanController.cs
+++ Assets/UnityChanController.cs
@@ -6,6 +6,10 @@ public class UnityChanController : MonoBehaviour
 {
     // アニメーションするためのコンポーネントを入れる
     private Animator myAnimator;
+    // Unityちゃんを移動させるコンポーネントを入れる（追加）
+    private Rigidbody myRigidbody;
+    // 前方向の速度（追加）
+    private float velocityZ = 16f;
 
     // Start is called before the first frame update
     void Start()
@@ -15,11 +19,15 @@ public class UnityChanController : MonoBehaviour
 
         // 走るアニメーションを開始
         this.myAnimator.SetFloat("Speed", 1);
+
+        // Rigidbodyコンポーネントを取得（追加）
+        this.myRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Unityちゃんに速度を与える（追加）
+        this.myRigidbody.velocity = new Vector3(0, 0, this.velocityZ);
     }
 }
