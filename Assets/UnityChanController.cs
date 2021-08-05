1622211b940882185b3c726362c82e5dcfcd3e80
syrsas <87002583+syrsas@users.noreply.github.com>
2021-08-06 00:50:29 +0900

    ファイル作成
    チャプター５まで

--- Assets/UnityChanController.cs
+++ Assets/UnityChanController.cs
@@ -10,6 +10,12 @@ public class UnityChanController : MonoBehaviour
     private Rigidbody myRigidbody;
     // 前方向の速度（追加）
     private float velocityZ = 16f;
+    // 横方向の速度（追加）
+    private float velocityX = 10f;
+    // 上方向の速度（追加）
+    private float velocityY = 10f;
+    // 左右の移動できる範囲（追加）
+    private float movableRange = 3.4f;
 
     // Start is called before the first frame update
     void Start()
@@ -27,7 +33,43 @@ public class UnityChanController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //横方向の入力による速度（追加）
+        float inputVelocityX = 0;
+        //上方向の入力による速度（追加）
+        float inputVelocityY = 0;
+
+        //Unityちゃんを矢印キーまたはボタンに応じて左右に移動させる（追加）
+        if (Input.GetKey(KeyCode.LeftArrow) && -this.movableRange < this.transform.position.x)
+        {
+            //左方向への速度を代入（追加）
+            inputVelocityX = -this.velocityX;
+        }
+        else if(Input.GetKey(KeyCode.RightArrow) && this.transform.position.x < this.movableRange)
+        {
+            //右方向への速度を代入（追加）
+            inputVelocityX = this.velocityX;
+        }
+
+        //ジャンプしていない時にスペースが押されたらジャンプする（追加）
+        if(Input.GetKeyDown(KeyCode.Space) && this.transform.position.y < 0.5f)
+        {
+            //ジャンプアニメを再生（追加）
+            this.myAnimator.SetBool("Jump", true);
+            //上方向への速度を代入（追加）
+            inputVelocityY = this.velocityY;
+        }
+        else
+        {
+            //現在のY軸の速度を代入（追加）
+            inputVelocityY = this.myRigidbody.velocity.y;
+        }
+
+        //Jumpステートの場合はJumpにfalseをセットする（追加）
+        if (this.myAnimator.GetCurrentAnimatorStateInfo(0).IsName("Jump"))
+        {
+            this.myAnimator.SetBool("Jump", false);
+        }
         //Unityちゃんに速度を与える（追加）
-        this.myRigidbody.velocity = new Vector3(0, 0, this.velocityZ);
+        this.myRigidbody.velocity = new Vector3(inputVelocityX, inputVelocityY, this.velocityZ);
     }
 }
