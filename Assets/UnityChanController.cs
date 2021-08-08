44dde1e9bafe854acb557f43439d20acd3b256a2
syrsas <87002583+syrsas@users.noreply.github.com>
2021-08-08 15:35:36 +0900

    ファイル作成
    レッスン６のチャプター１１まで作成しました。

--- Assets/UnityChanController.cs
+++ Assets/UnityChanController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;   //追加
 
 public class UnityChanController : MonoBehaviour
 {
@@ -16,6 +17,22 @@ public class UnityChanController : MonoBehaviour
     private float velocityY = 10f;
     // 左右の移動できる範囲（追加）
     private float movableRange = 3.4f;
+    // 動きを減速させる係数（追加）
+    private float coefficient = 0.99f;
+    // ゲーム終了の判定（追加）
+    private bool isEnd = false;
+    // ゲーム終了時に表示するテキスト（追加）
+    private GameObject stateText;
+    //スコアを表示するテキスト（追加）
+    private GameObject scoreText;
+    //得点（追加）
+    private int score = 0;
+    //左ボタン押下の判定（追加）
+    private bool isLButtonDown = false;
+    //右ボタン押下の判定（追加）
+    private bool isRButtonDown = false;
+    //ジャンプボタン押下の判定（追加）
+    private bool isJButtonDown = false;
 
     // Start is called before the first frame update
     void Start()
@@ -28,30 +45,45 @@ public class UnityChanController : MonoBehaviour
 
         // Rigidbodyコンポーネントを取得（追加）
         this.myRigidbody = GetComponent<Rigidbody>();
+
+        // シーン中のstateTextオブジェクトを取得（追加）
+        this.stateText = GameObject.Find("GameResultText");
+
+        // シーン中のscoreTextオブジェクトを取得（追加）
+        this.scoreText = GameObject.Find("ScoreText");
     }
 
     // Update is called once per frame
     void Update()
     {
+        //ゲーム終了ならUnityちゃんの動きを減衰する（追加）
+        if (this.isEnd)
+        {
+            this.velocityZ *= this.coefficient;
+            this.velocityX *= this.coefficient;
+            this.velocityY *= this.coefficient;
+            this.myAnimator.speed *= this.coefficient;
+        }
+
         //横方向の入力による速度（追加）
         float inputVelocityX = 0;
         //上方向の入力による速度（追加）
         float inputVelocityY = 0;
 
         //Unityちゃんを矢印キーまたはボタンに応じて左右に移動させる（追加）
-        if (Input.GetKey(KeyCode.LeftArrow) && -this.movableRange < this.transform.position.x)
+        if ((Input.GetKey(KeyCode.LeftArrow) || this.isLButtonDown) && -this.movableRange < this.transform.position.x)
         {
             //左方向への速度を代入（追加）
             inputVelocityX = -this.velocityX;
         }
-        else if(Input.GetKey(KeyCode.RightArrow) && this.transform.position.x < this.movableRange)
+        else if((Input.GetKey(KeyCode.RightArrow) || this.isRButtonDown) && this.transform.position.x < this.movableRange)
         {
             //右方向への速度を代入（追加）
             inputVelocityX = this.velocityX;
         }
 
         //ジャンプしていない時にスペースが押されたらジャンプする（追加）
-        if(Input.GetKeyDown(KeyCode.Space) && this.transform.position.y < 0.5f)
+        if((Input.GetKeyDown(KeyCode.Space) || this.isJButtonDown) && this.transform.position.y < 0.5f)
         {
             //ジャンプアニメを再生（追加）
             this.myAnimator.SetBool("Jump", true);
@@ -71,5 +103,67 @@ public class UnityChanController : MonoBehaviour
         }
         //Unityちゃんに速度を与える（追加）
         this.myRigidbody.velocity = new Vector3(inputVelocityX, inputVelocityY, this.velocityZ);
+    }
+
+    //トリガーモードで他のオブジェクトと接触した場合の処理（追加）
+    private void OnTriggerEnter(Collider other)
+    {
+        //障害物に衝突した場合（追加）
+        if(other.gameObject.tag == "CarTag" || other.gameObject.tag == "TrafficConeTag")
+        {
+            this.isEnd = true;
+            //stateTextにGAME OVERを表示（追加）
+            this.stateText.GetComponent<Text>().text = "GAME OVER";
+        }
+        //ゴール地点に到達した場合（追加）
+        if(other.gameObject.tag == "GoalTag")
+        {
+            this.isEnd = true;
+            //stateTextにGAME CLEARを表示（追加）
+            this.stateText.GetComponent<Text>().text = "CLEAR!!";
+        }
+        //コインに衝突した場合（追加）
+        if(other.gameObject.tag == "CoinTag")
+        {
+            //スコアを加算（追加）
+            this.score += 10;
+            //ScoreText獲得した点数を表示（追加）
+            this.scoreText.GetComponent<Text>().text = "Score " + this.score + "pt";
+            //パーティクルを再生（追加）
+            GetComponent<ParticleSystem>().Play();
+            //接触したコインのオブジェクトを破棄（追加）
+            Destroy(other.gameObject);
+        }
+    }
+
+    //ジャンプボタンを押した場合の処理（追加）
+    public void GetMyJumpButtonDown()
+    {
+        this.isJButtonDown = true;
+    }
+    //ジャンプボタンを離した場合の処理（追加）
+    public void GetMyJumpButtonUp()
+    {
+        this.isJButtonDown = false;
+    }
+    //左ボタンを押し続けた場合の処理（追加）
+    public void GetMyLeftButtonDown()
+    {
+        this.isLButtonDown = true;
+    }
+    //左ボタンを離した場合の処理（追加）
+    public void GetMyLeftButtonUp()
+    {
+        this.isLButtonDown = false;
+    }
+    //右ボタンを押し続けた場合の処理（追加）
+    public void GetMyRightButtonDown()
+    {
+        this.isRButtonDown = true;
+    }
+    //右ボタンを離した場合の処理（追加）
+    public void GetMyRightButtonUp()
+    {
+        this.isRButtonDown = false;
     }
 }
