812d9a81937f3100c1ff3448dd78ccd538e492ce
syrsas <87002583+syrsas@users.noreply.github.com>
2021-08-09 00:57:50 +0900

    ファイル作成
    発展課題に挑戦したらアイテムが生成されなくなりました。

--- Assets/ItemGenerator.cs
+++ Assets/ItemGenerator.cs
@@ -16,15 +16,47 @@ public class ItemGenerator : MonoBehaviour
     private int goalPos = 360;
     //アイテムを出すx方向の範囲
     private float posRange = 3.4f;
+    // Unityちゃんのオブジェクト（発展）
+    private GameObject unitychan;
+    // Unityちゃんとカメラの距離（発展）
+    private float difference;
 
     // Start is called before the first frame update
     void Start()
     {
-        //一定の距離ごとにアイテムを生成
-        for(int i = startPos; i < goalPos; i += 15)
-        {
+        // Unityちゃんのオブジェクトを取得（発展）
+        this.unitychan = GameObject.Find("unitychan");
+        // Unityちゃんとカメラの位置（z座標）の差を求める（発展）
+        this.difference = unitychan.transform.position.z - this.transform.position.z;
+
+        //Unityちゃんの進行に応じてアイテムを生成
+        
+            if (this.transform.position.z >= Camera.main.transform.position.z)
+            {
+                //どのアイテムを出すかをランダムに設定
+                int num = Random.Range(1, 11);
+                if (num >= 1 && num <= 4)
+                {
+                    GameObject cone = Instantiate(conePrefab);
+                    cone.transform.position = new Vector3(4 * cone.transform.position.x, cone.transform.position.y, cone.transform.position.z);
+                }
+                else if (num >= 5 && num <= 8)
+                {
+                    //コインを生成
+                    GameObject coin = Instantiate(coinPrefab);
+                    coin.transform.position = new Vector3(coin.transform.position.x, coin.transform.position.y, coin.transform.position.z);
+
+                }
+                else if (num >= 9 && num <= 11)
+                {
+                    //車を生成
+                    GameObject car = Instantiate(carPrefab);
+                    car.transform.position = new Vector3(car.transform.position.x, car.transform.position.y, car.transform.position.z);
+
+                }
+            }
             //どのアイテムを出すかをランダムに設定
-            int num = Random.Range(1, 11);
+            /*int num = Random.Range(1, 11);
             if(num <= 2)
             {
                 //コーンをx軸方向に一直線に生成
@@ -33,8 +65,8 @@ public class ItemGenerator : MonoBehaviour
                     GameObject cone = Instantiate(conePrefab);
                     cone.transform.position = new Vector3(4 * j, cone.transform.position.y, i);
                 }
-            }
-            else
+            }*/
+            /*else
             {
                 //レーンごとにアイテムを生成
                 for(int j = -1; j <= 1; j++)
@@ -58,8 +90,8 @@ public class ItemGenerator : MonoBehaviour
                         car.transform.position = new Vector3(posRange * j, car.transform.position.y, i + offsetZ);
                     }
                 }
-            }
-        }
+            }*/
+        
         
     }
 
